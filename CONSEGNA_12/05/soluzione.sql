55d5142d8546fdf3ece0f1946eb115a3a3fb32a2
StefanoM98 <smartino030@gmail.com>
2023-05-12 14:11:50 +0200

    Add: Risposta 5-6-7

--- CONSEGNA_12/05/soluzione.sql
+++ CONSEGNA_12/05/soluzione.sql
@@ -20,11 +20,20 @@
     FROM `courses`
     WHERE period = 'I semestre' AND year = 1
 
-5.	Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
+5.	Selezionare tutti gli appelli desame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
+    SELECT * 
+    FROM `exams`
+    WHERE (`hour` BETWEEN "14:00:00" AND "23:59:59") AND (`date` = "2020/06/20");
 
 6.	Selezionare tutti i corsi di laurea magistrale (38)
+    SELECT * 
+    FROM `degrees`
+    WHERE `level` = "magistrale";
 
-7.	Da quanti dipartimenti è composta l'università? (12)
+
+7.	Da quanti dipartimenti è composta luniversità? (12)
+    SELECT COUNT(*) as `departments_num`
+    FROM departments
 
 8.	Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
 
