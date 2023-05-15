6c46627701d8718c47b8d4114f0d79b6ac0cad62
StefanoM98 <smartino030@gmail.com>
2023-05-15 15:19:28 +0200

    Add: risposta 1-2-3 sezione join

--- CONSEGNA 15-5/soluzione_join.sql
+++ CONSEGNA 15-5/soluzione_join.sql
@@ -1,6 +1,31 @@
 -- 1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
+
+SELECT `students`.`name`, `students`.`surname` 
+FROM `degrees` 
+JOIN `students` 
+ON `degrees`.`id` = `students`.`degree_id` 
+WHERE `degrees`.`name` = "Corso di Laurea in Economia";
+
 -- 2. Selezionare tutti i Corsi di Laurea Magistrale del Dipartimento di Neuroscienze
+
+SELECT `degrees`.`name`, `degrees`.`level` 
+FROM `degrees` 
+JOIN `departments` 
+ON `degrees`.`department_id`= `departments`.`id` 
+WHERE `departments`.`name`= "Dipartimento di Neuroscienze" 
+AND `degrees`.`level`= "magistrale";
+
 -- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
+
+SELECT `teachers`.`name`AS `teacher_name`, `teachers`.`surname` AS `teacher_surname`, `courses`.`name` AS `course_name`
+FROM `teachers`
+JOIN `course_teacher`
+ON `teachers`.`id` = `course_teacher`.`teacher_id`
+JOIN `courses`
+ON `courses`.`id` = `course_teacher`.`course_id`
+WHERE `teachers`.`id` = 44
+
+
 -- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
 -- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
 -- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
