6a80748601efdb4b7dccfb6472145d8221331b0c
StefanoM98 <smartino030@gmail.com>
2023-05-15 15:45:01 +0200

    Add: risposta 4-5 sezione join

--- CONSEGNA 15-5/soluzione_join.sql
+++ CONSEGNA 15-5/soluzione_join.sql
@@ -27,6 +27,26 @@ WHERE `teachers`.`id` = 44
 
 
 -- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
+
+SELECT `students`.`name`, `students`.`surname`, `degrees`.`name`, `departments`.`name`
+FROM `students`
+JOIN `degrees`
+ON `students`.`degree_id` = `degrees`.`id`
+JOIN `departments`
+ON `degrees`.`department_id`= `departments`.`id`
+ORDER BY `students`.`surname` ASC
+
 -- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
+
+SELECT `degrees`.`name` AS `degree_name`, `courses`.`name` as `courses_name`, `teachers`.`name` as `teacher_name`, `teachers`.`surname` AS `teachers_surname`
+FROM `degrees`
+JOIN `courses`
+ON `degrees`.`id` = `courses`.`degree_id`
+JOIN `course_teacher`
+ON `course_teacher`.`course_id` = `courses`.`id`
+JOIN `teachers`
+ON `teachers`.`id` = `course_teacher`.`teacher_id`
+
+
 -- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
 -- 7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per superare ciascuno dei suoi esami
